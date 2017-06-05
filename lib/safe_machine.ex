ecd9795a3931dbb64ddeb1fa46109e86235a975c
Joey Pereira <joey@pereira.io>
2017-06-05 13:13:03 -0400

    initial elixir base

---
+++ lib/safe_machine.ex
@@ -0,0 +1,18 @@
+defmodule SafeMachine do
+  @moduledoc """
+  Documentation for SafeMachine.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SafeMachine.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/safe_machine_test.exs
@@ -0,0 +1,8 @@
+defmodule SafeMachineTest do
+  use ExUnit.Case
+  doctest SafeMachine
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
