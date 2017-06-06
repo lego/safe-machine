e2a84e043327fce7c209cb4b36d1a2c1d1654a7e
Joey Pereira <joey@pereira.io>
2017-06-05 22:32:48 -0400

    Working basic transition macros

--- lib/safe_machine.ex
+++ lib/safe_machine.ex
@@ -1,18 +1,57 @@
 defmodule SafeMachine do
-  @moduledoc """
-  Documentation for SafeMachine.
-  """
 
-  @doc """
-  Hello world.
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      require Logger
+      import SafeMachine
+      Module.register_attribute(__MODULE__, :state_transitions, accumulate: true)
+    end
+  end
 
-  ## Examples
+  defmacro safe_machine_finish do
+    quote do
+      def get_state_transitions do
+        @state_transitions
+      end
+      def get_env do
+        __ENV__
+      end
+    end
+  end
 
-      iex> SafeMachine.hello
-      :world
+  @doc false
+  defmacro transition(from, to: to) do
+    quote do
+      Module.put_attribute(__MODULE__, :state_transitions, [unquote(from), unquote(to)])
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defmacro transition(from, [to: to], clause) do
+    # IO.inspect(Enum.map(Keyword.get_values(clause, :rescue), fn v -> {:rescue, v} end))
+    # IO.inspect(Code.compile_string("rescue e in RunetimeError -> e"))
+    # IO.inspect(clause[:catch])
+    quote do
+      Module.put_attribute(__MODULE__, :state_transitions, [unquote(from), unquote(to)])
+      def do_transition(%__MODULE__{state: unquote(from)} = self, unquote(from) = from, unquote(to) = to) do
+        Logger.debug("TRANSITION MACRO: do_transition " <> Atom.to_string(from) <> " ~> " <> Atom.to_string(to))
+        Logger.debug("SELF:  " <> inspect(self))
+        result = try do
+          unquote(clause[:do])
+          Logger.debug("DO FINISHED")
+          %{self | state: to}
+        catch
+          unquote(clause[:catch] || [])
+        after
+          unquote(clause[:after])
+          Logger.debug("AFTER FINISHED")
+        end
+        Logger.debug("TRANSITION MACRO: end " <> Atom.to_string(from) <> " ~> " <> Atom.to_string(to))
+        case result do
+          %__MODULE__{state: to} = self -> {:ok, self}
+          _ -> {:error, result}
+        end
+      end
+    end
   end
 end
--- test/safe_machine_test.exs
+++ test/safe_machine_test.exs
@@ -1,8 +1,51 @@
+defmodule Dog do
+  use SafeMachine
+
+  # Defines the struct %Dog{}
+  @enforce_keys [:name]
+  defstruct name: nil, state: :puppy
+  @type t :: %Dog{name: String.t, state: Atom}
+
+  transition :puppy, to: :dog do
+    IO.puts("bark")
+  end
+
+  transition :dog, to: :wolf do
+    IO.puts("Hmm, I'm still a dog.")
+    throw [:not_a_wolf, "Too young"]
+  catch
+    [:not_a_wolf, reason] ->
+      IO.puts "SAFE ERROR: not_a_wolf, #{reason}."
+  after
+    IO.puts("after woof")
+  end
+
+  safe_machine_finish()
+end
+
 defmodule SafeMachineTest do
   use ExUnit.Case
   doctest SafeMachine
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  require Logger
+
+  test "macro definition" do
+
+    doge = %Dog{name: "JC"}
+    Logger.info("Initial struct: " <> inspect(doge))
+    Logger.info("Dog state transitions: " <> inspect(Dog.get_state_transitions))
+
+    # IO.inspect(Dog.__info__(:functions))
+    # IO.inspect(Dog.get_env)
+    {:ok, doge} = Dog.do_transition(doge, :puppy, :dog)
+    {:error, doge} = Dog.do_transition(doge, :dog, :wolf)
+
+    # > not_doge = 1
+    # > doge = Dog.do_transition(no_doge, :dog, :wolf)
+    # => emits No Function Available error
+    #
+    # > doge = Dog.do_transition(doge, :dog, :wolf)
+    # => emits No Function Available error becaus
+    Logger.info("End Dog: " <> inspect(doge))
   end
 end
