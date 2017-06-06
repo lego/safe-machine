d3849b674bd251f24b629bde89a556e46cef5e0a
Joey Pereira <joey@pereira.io>
2017-06-06 01:15:09 -0400

    Make macro more concise and better analysis

--- lib/safe_machine.ex
+++ lib/safe_machine.ex
@@ -5,53 +5,81 @@ defmodule SafeMachine do
     quote do
       require Logger
       import SafeMachine
-      Module.register_attribute(__MODULE__, :state_transitions, accumulate: true)
+      Module.put_attribute(__MODULE__, :state_transitions, %{})
+      @on_definition SafeMachine
     end
   end
 
+  @doc false
   defmacro safe_machine_finish do
     quote do
+      # FIXME: replace this with an after_compile type check for transition consistency
+      # but we still want this at runtime :(
       def get_state_transitions do
         @state_transitions
-      end
-      def get_env do
-        __ENV__
       end
     end
   end
 
   @doc false
-  defmacro transition(from, to: to) do
-    quote do
-      Module.put_attribute(__MODULE__, :state_transitions, [unquote(from), unquote(to)])
-    end
+  def __on_definition__(_env, :def, :do_transition, args, _guards, _body) do
+    {:=, _, [from_state | _]} = Enum.at(args, 1)
+    {:=, _, [to_state | _]} = Enum.at(args, 2)
+    IO.puts "Defining transition from #{from_state} to #{to_state}."
+    # FIXME: do static analysis here of any circular transitions
   end
 
-  defmacro transition(from, [to: to], clause) do
+  @doc false
+  def __on_definition__(_env, _kind, _name, _args, _guards, _body) do
+    # Serves as a catchall for on_definition
+  end
+
+  @doc false
+  defmacro deftransition({:=, _, [{:%, _, [{:__aliases__, _, [_]}, {:%{}, _, struct_args}]}, struct_self]}, [to: to], clause \\ []) do
     # IO.inspect(Enum.map(Keyword.get_values(clause, :rescue), fn v -> {:rescue, v} end))
     # IO.inspect(Code.compile_string("rescue e in RunetimeError -> e"))
-    # IO.inspect(clause[:catch])
+    from = if Keyword.keyword?(struct_args) && struct_args[:state] do
+      struct_args[:state]
+    else
+      raise "\"state:\" not provided in expansion"
+    end
+
     quote do
-      Module.put_attribute(__MODULE__, :state_transitions, [unquote(from), unquote(to)])
-      def do_transition(%__MODULE__{state: unquote(from)} = self, unquote(from) = from, unquote(to) = to) do
+      Module.put_attribute(__MODULE__, :state_transitions, Map.put(Module.get_attribute(__MODULE__, :state_transitions), unquote(from), unquote(to))) # we can't include unquote(struct_args) due to unbound variables
+
+      def do_transition(%__MODULE__{unquote_splicing(struct_args)} = unquote(struct_self), unquote(from) = from, unquote(to) = to) do
         Logger.debug("TRANSITION MACRO: do_transition " <> Atom.to_string(from) <> " ~> " <> Atom.to_string(to))
-        Logger.debug("SELF:  " <> inspect(self))
+        Logger.debug("SELF:  " <> inspect(unquote(struct_self)))
         result = try do
-          unquote(clause[:do])
+          transition_outcome = unquote(clause[:do])
           Logger.debug("DO FINISHED")
-          %{self | state: to}
+          case transition_outcome do
+            %__MODULE__{state: unquote(from)} = result -> %{result | state: to}
+            %__MODULE__{state: unexpected_state} -> raise "Did not expect change of :state during #{__MODULE__}.transition from :#{unquote(from)} to :#{unquote(to)}: got %#{__MODULE__}{state: #{unexpected_state}}"
+            _ -> %{unquote(struct_self) | state: to}
+          end
         catch
-          unquote(clause[:catch] || [])
+          # FIXME: how to handle catch with default x -> x
+          # unquote(clause[:catch])
+          x -> x
         after
-          unquote(clause[:after])
+          # FIXME: properly handle clause and defaulted, or whether to allow after
+          # unquote(clause[:after])
           Logger.debug("AFTER FINISHED")
         end
         Logger.debug("TRANSITION MACRO: end " <> Atom.to_string(from) <> " ~> " <> Atom.to_string(to))
         case result do
-          %__MODULE__{state: to} = self -> {:ok, self}
+          %__MODULE__{state: to} = unquote(struct_self) -> {:ok, unquote(struct_self)}
           _ -> {:error, result}
         end
       end
     end
   end
+
+  defmacro unsafe(do: do_clause) do
+    quote do
+      unquote(do_clause)
+    end
+  end
+
 end
--- test/safe_machine_test.exs
+++ test/safe_machine_test.exs
@@ -1,23 +1,51 @@
-defmodule Dog do
+defmodule Animal do
   use SafeMachine
 
-  # Defines the struct %Dog{}
   @enforce_keys [:name]
-  defstruct name: nil, state: :puppy
-  @type t :: %Dog{name: String.t, state: Atom}
+  defstruct name: nil, state: :puppy, level: 1
+  @type t :: %Animal{name: String.t, state: Atom, level: Integer.t}
 
-  transition :puppy, to: :dog do
-    IO.puts("bark")
+  deftransition(%Animal{state: :puppy, name: "Modify", level: level} = dog, to: :dog) do
+    %{dog | level: level + 1}
   end
 
-  transition :dog, to: :wolf do
-    IO.puts("Hmm, I'm still a dog.")
+  deftransition(%Animal{state: :puppy, name: "Bad"} = dog, to: :dog) do
+    %{dog | state: :evil}
+  end
+
+  deftransition(%Animal{state: :puppy, name: "Conditional", level: level} = dog, to: :dog) do
+    if level == 1 do
+      throw [:something_happened]
+    else
+      %{dog | level: level + 1}
+    end
+  end
+
+  deftransition(%Animal{state: :puppy, name: "Bad Premature"} = dog, to: :dog) do
+    %{dog | state: :dog}
+  end
+
+  deftransition(%Animal{state: :puppy, name: "Empty block"} = dog, to: :dog)
+
+  # FIXME: how do we safely do side effects?
+  deftransition(%Animal{state: :puppy, name: "Side effects"} = dog, to: :dog) do
+    unsafe do
+      IO.puts("Side effects")
+    end
+  end
+
+  deftransition(%Animal{state: :puppy} = dog, to: :dog)
+
+  # FIXME: not yet handled, use of special catch conditions
+  deftransition(%Animal{state: :dog, name: "Handled throw"} = dog, to: :wolf) do
     throw [:not_a_wolf, "Too young"]
   catch
     [:not_a_wolf, reason] ->
       IO.puts "SAFE ERROR: not_a_wolf, #{reason}."
-  after
-    IO.puts("after woof")
+  end
+
+  deftransition(%Animal{state: :dog, name: "Unhandled throw"} = dog, to: :wolf) do
+    throw [:not_a_wolf, "Too young"]
   end
 
   safe_machine_finish()
@@ -29,23 +57,61 @@ defmodule SafeMachineTest do
 
   require Logger
 
-  test "macro definition" do
-
-    doge = %Dog{name: "JC"}
-    Logger.info("Initial struct: " <> inspect(doge))
-    Logger.info("Dog state transitions: " <> inspect(Dog.get_state_transitions))
-
-    # IO.inspect(Dog.__info__(:functions))
-    # IO.inspect(Dog.get_env)
-    {:ok, doge} = Dog.do_transition(doge, :puppy, :dog)
-    {:error, doge} = Dog.do_transition(doge, :dog, :wolf)
-
-    # > not_doge = 1
-    # > doge = Dog.do_transition(no_doge, :dog, :wolf)
-    # => emits No Function Available error
-    #
-    # > doge = Dog.do_transition(doge, :dog, :wolf)
-    # => emits No Function Available error becaus
-    Logger.info("End Dog: " <> inspect(doge))
+  test "state transitions" do
+    assert %{dog: :wolf, puppy: :dog} == Animal.get_state_transitions
   end
+
+  test "transition modifies contents" do
+    doge = %Animal{name: "Modify", level: 2}
+    assert {:ok, %Animal{name: "Modify", level: 3, state: :dog}} == Animal.do_transition(doge, :puppy, :dog)
+  end
+
+  test "failure when transition mutates state" do
+    doge = %Animal{name: "Bad"}
+    assert_raise RuntimeError, ~r/Did not expect/, fn ->
+      Animal.do_transition(doge, :puppy, :dog)
+    end
+  end
+
+  test "failure when transition mutates state to destination state" do
+    doge = %Animal{name: "Bad Premature"}
+    assert_raise RuntimeError, ~r/Did not expect/, fn ->
+      Animal.do_transition(doge, :puppy, :dog)
+    end
+  end
+
+  test "transition changing behaviour" do
+    doge = %Animal{name: "Conditional", level: 2}
+    assert {:ok, %Animal{name: "Conditional", level: 3, state: :dog}} == Animal.do_transition(doge, :puppy, :dog)
+
+    doge = %Animal{name: "Conditional", level: 1}
+    assert {:error, [:something_happened]} == Animal.do_transition(doge, :puppy, :dog)
+  end
+
+  test "transition with empty block" do
+    doge = %Animal{name: "Empty block"}
+    assert {:ok, %Animal{name: "Empty block", state: :dog}} == Animal.do_transition(doge, :puppy, :dog)
+  end
+
+  test "transition with side effects" do
+    doge = %Animal{name: "Empty block"}
+    assert {:ok, %Animal{name: "Empty block", state: :dog}} == Animal.do_transition(doge, :puppy, :dog)
+  end
+
+  #   # Logger.info("Initial struct: " <> inspect(doge))
+  #   # Logger.info("Animal state transitions: " <> inspect(Animal.get_state_transitions))
+  #
+  #   # IO.inspect(Animal.__info__(:functions))
+  #   # IO.inspect(Animal.get_env)
+  #   {:ok, doge} = Animal.do_transition(doge, :puppy, :dog)
+  #   {:error, doge} = Animal.do_transition(doge, :dog, :wolf)
+  #
+  #   # > not_doge = 1
+  #   # > doge = Animal.do_transition(no_doge, :dog, :wolf)
+  #   # => emits No Function Available error
+  #   #
+  #   # > doge = Animal.do_transition(doge, :dog, :wolf)
+  #   # => emits No Function Available error becaus
+  #   Logger.info("End Animal: " <> inspect(doge))
+  # end
 end
