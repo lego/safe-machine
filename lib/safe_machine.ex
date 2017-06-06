2dd3884027beaa88fbe41472f28ad160cc63cd86
Joey Pereira <joey@pereira.io>
2017-06-06 01:37:33 -0400

    Remove hardcoded connection between transitioning struct and parent module

--- lib/safe_machine.ex
+++ lib/safe_machine.ex
@@ -35,7 +35,7 @@ defmodule SafeMachine do
   end
 
   @doc false
-  defmacro deftransition({:=, _, [{:%, _, [{:__aliases__, _, [_]}, {:%{}, _, struct_args}]}, struct_self]}, [to: to], clause \\ []) do
+  defmacro deftransition({:=, _, [{:%, _, [struct_name, {:%{}, _, struct_args}]}, struct_self]}, [to: to], clause \\ []) do
     # IO.inspect(Enum.map(Keyword.get_values(clause, :rescue), fn v -> {:rescue, v} end))
     # IO.inspect(Code.compile_string("rescue e in RunetimeError -> e"))
     from = if Keyword.keyword?(struct_args) && struct_args[:state] do
@@ -47,15 +47,16 @@ defmodule SafeMachine do
     quote do
       Module.put_attribute(__MODULE__, :state_transitions, Map.put(Module.get_attribute(__MODULE__, :state_transitions), unquote(from), unquote(to))) # we can't include unquote(struct_args) due to unbound variables
 
-      def do_transition(%__MODULE__{unquote_splicing(struct_args)} = unquote(struct_self), unquote(from) = from, unquote(to) = to) do
+      # FIXME: handle when gaurds
+      def do_transition(%unquote(struct_name){unquote_splicing(struct_args)} = unquote(struct_self), unquote(from) = from, unquote(to) = to) do
         Logger.debug("TRANSITION MACRO: do_transition " <> Atom.to_string(from) <> " ~> " <> Atom.to_string(to))
         Logger.debug("SELF:  " <> inspect(unquote(struct_self)))
         result = try do
           transition_outcome = unquote(clause[:do])
           Logger.debug("DO FINISHED")
           case transition_outcome do
-            %__MODULE__{state: unquote(from)} = result -> %{result | state: to}
-            %__MODULE__{state: unexpected_state} -> raise "Did not expect change of :state during #{__MODULE__}.transition from :#{unquote(from)} to :#{unquote(to)}: got %#{__MODULE__}{state: #{unexpected_state}}"
+            %unquote(struct_name){state: unquote(from)} = result -> %{result | state: to}
+            %unquote(struct_name){state: unexpected_state} -> raise "Did not expect change of :state during #{__MODULE__}.do_transition from :#{unquote(from)} to :#{unquote(to)}: got %#{unquote(struct_name)}{state: #{unexpected_state}}"
             _ -> %{unquote(struct_self) | state: to}
           end
         catch
@@ -69,7 +70,7 @@ defmodule SafeMachine do
         end
         Logger.debug("TRANSITION MACRO: end " <> Atom.to_string(from) <> " ~> " <> Atom.to_string(to))
         case result do
-          %__MODULE__{state: to} = unquote(struct_self) -> {:ok, unquote(struct_self)}
+          %unquote(struct_name){state: to} = unquote(struct_self) -> {:ok, unquote(struct_self)}
           _ -> {:error, result}
         end
       end
